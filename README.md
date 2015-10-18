4adbaf644a70eb1630081047492b0be8ea033f21
Ilya Stupakov <istupakov@gmail.com>
2015-10-18 06:17:53 +0600

    Configure build process with grunt.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Manga Downloader
-Chrome extension for download manga to zip archives.
+> Chrome extension for download manga to zip archives.
+
+For install project dependencies use commands
+```shell
+npm install -g bower 
+npm install -g tsd
+npm install -g typescript
+npm install -g grunt-cli
+npm install
+grunt install
+```
+
+For build project use command
+```shell
+grunt
+```
---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-tsd");
+  grunt.loadNpmTasks("grunt-bower-install-simple");
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.initConfig({
+    ts: {
+      default: {
+        files: {
+          'release/manga.js': ['src/*.ts'],
+        },
+        tsconfig: true,
+        options: {
+          inlineSources: true,
+        }
+      }
+    },
+    tsd: {
+      refresh: {
+        options: {
+          command: 'reinstall',
+          latest: true,
+          config: 'tsd.json',
+        }
+      }
+    },
+    "bower-install-simple": {
+      options: { color: true, directory: "lib" },
+      prod: {
+        options: { production: true }
+      },
+    },
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: ['src/*.js', 'src/*.html', 'src/*.json', 'icons/*.png', 'lib/jquery/dist/jquery.min.js'],
+          dest: 'release/'
+        }, {
+            expand: true,
+            flatten: true,
+            src: ['lib/zip.js/WebContent/*.js'],
+            dest: 'release/zip.js'
+          }]
+      }
+    }
+  });
+
+  grunt.registerTask("install", ["tsd", "bower-install-simple"]);
+  grunt.registerTask("default", ["ts", "copy"]);
+};
